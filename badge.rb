8e1e285a234ceadfa7dd7cfa93d968be6d5130a7
Bart Kamphorst <bart@kamphorst.com>
2020-02-11 10:22:42 +0100

    Add optional number argument to indicate how many dependencies are out-of-date. Make label width variable per badge type.

--- badge.rb
+++ badge.rb
@@ -3,24 +3,28 @@ require 'victor'
 BADGE_OPTIONS = {
   up_to_date: {
     bg_color: '#32CD32',
-    text: 'up-to-date'
+    text: 'up-to-date',
+    width: 180
   },
   out_of_date: {
     bg_color: '#ff0000',
-    text: 'out-of-date'    
+    text: 'out-of-date',
+    width: 190
   },
   unknown: {
     bg_color: '#666',
-    text: 'unknown'
+    text: 'unknown',
+    width: 170
   }
 }
 
-def build_badge(status)
+def build_badge(status, num=nil)
   if ! [:up_to_date, :out_of_date, :unknown].include?(status)
     status = :unknown
   end
-  
-  svg = Victor::SVG.new width: 180, height: 32
+  number = Integer(num) rescue nil if num
+
+  svg = Victor::SVG.new width: BADGE_OPTIONS[status][:width], height: 32
 
   style = {
     stroke: '#d3d3d3',
@@ -28,10 +32,10 @@ def build_badge(status)
   }
 
   svg.build do
-    rect x: 0, y: 0, width: 180, height: 32, fill: BADGE_OPTIONS[status][:bg_color], style: style
+    rect x: 0, y: 0, width: BADGE_OPTIONS[status][:width], height: 32, fill: BADGE_OPTIONS[status][:bg_color], style: style
 
     g font_size: 14, font_family: 'arial', fill: 'white' do
-      text "Dependencies #{BADGE_OPTIONS[status][:text]}", x: 10, y: 20
+      text "#{number} Dependencies #{BADGE_OPTIONS[status][:text]}", x: 10, y: 20
     end
   end
   return svg
@@ -39,7 +43,7 @@ end
 
 positive_badge = build_badge(:up_to_date)
 positive_badge.save 'uptodate' # Or use positive_badge.render
-negative_badge = build_badge(:out_of_date)
+negative_badge = build_badge(:out_of_date, 6)
 negative_badge.save 'outdated'
 unknown_badge = build_badge(:foo)
 unknown_badge.save 'unknown'

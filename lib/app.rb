d270dad8434669afd38dcce2611ee3cb15e94456
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-02-15 17:19:16 +0100

    Separate app and launch

--- lib/app.rb
+++ lib/app.rb
@@ -5,10 +5,8 @@ require File.expand_path('../workers/badge.rb', __FILE__)
 require 'sucker_punch'
 require 'sinatra'
 require 'sinatra/logger'
-require 'yaml'
 require 'json'
 require 'moneta'
-require 'rufus-scheduler'
 
 module RubyDepsHelpers
   def worker_all_badges(repositories)
@@ -32,30 +30,7 @@ module RubyDepsHelpers
   end
 end
 
-config = <<YAML
-github:
-  gollum:
-    gollum:
-      api_token: secret
-    gollum-lib:
-      api_token: secret
-      locations: [gemspec.rb]
-      dependency_types: [runtime, development]
-  singingwolfboy:
-    flask-dance:
-      language: python
-gitlab:
-  cthowl01:
-    team-chess-ruby:
-      api_token: secret
-YAML
 
-options = {
-  :port => 4567,
-  :bind => '0.0.0.0',
-}
-
-store = Moneta.new(:Memory)
 
 class RubyDeps < Sinatra::Base
   include RubyDepsHelpers
@@ -96,40 +71,3 @@ class RubyDeps < Sinatra::Base
 
 end
 
-repositories = {}
-YAML.load(config).each do |source, orgs|
-  orgs.each do |org, value|
-    value.each do |name, settings|
-      cfg = settings.is_a?(Hash) ? settings : {}
-      repo = Object.const_get("#{source.capitalize}Repository").new(org, name, cfg.fetch('language', nil), cfg.fetch('locations', nil), cfg.fetch('branch', nil), cfg.fetch('api_token', nil))
-      repo.dependency_types = cfg['dependency_types'].map {|dep| dep.to_sym} if cfg['dependency_types'].is_a?(Array)
-      repositories["#{source}/#{org}/#{name}"] = repo
-    end
-  end
-end
-
-# Need to initialize the log like this once, because otherwise it only becomes available after the Sinatra app has received a request...
-::SemanticLogger.add_appender(file_name: "#{RubyDeps.environment}.log")
-
-RubyDeps.set(:repositories, repositories)
-RubyDeps.set(:store, store)
-RubyDeps.set(:enable_logging, true)
-
-puts "Scheduling Jobs..."
-scheduler = Rufus::Scheduler.new
-scheduler.every('1h') do
-  worker_fetch_all(repositories.values)
-end
-scheduler.every('1h5m') do
-  worker_all_badges(repositories.values)
-end
-
-puts "Running Workers a first time..."
-include RubyDepsHelpers
-worker_fetch_all(repositories.values)
-
-sleep 5
-worker_all_badges(repositories.values)
-
-puts "Starting Sinatra..."
-RubyDeps.run!(options)

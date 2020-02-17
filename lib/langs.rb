d652a86b397cf69a3bbc02d4bef502863660646e
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-02-17 18:18:06 +0100

    Fix Travis

--- lib/langs.rb
+++ lib/langs.rb
@@ -19,7 +19,7 @@ module LanguageHelpers
   end
 
   def log_error(message)
-    logger.error(message) if ::RubyDeps.enable_logging
+    logger.error(message) if defined?(::RubyDeps) && ::RubyDeps.enable_logging
   end
 end
 

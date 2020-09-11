40fd5dce2163b05e296811c09e8a3af696c5fc43
Dawa Ometto <dawa.ometto@phil.uu.nl>
2020-09-11 20:09:53 +0200

    Refactor

--- lib/cutting_edge/workers/badge.rb
+++ lib/cutting_edge/workers/badge.rb
@@ -4,16 +4,21 @@ module CuttingEdge
   BADGE_TEMPLATE = File.read(File.expand_path('../../templates/badge.svg.erb', __FILE__)) unless defined?(BADGE_TEMPLATE)
   BADGE_BASE_WIDTH = 35 unless defined?(BADGE_BASE_WIDTH)
   BADGE_CELL_WIDTH = 25 unless defined?(BADGE_CELL_WIDTH)
-  BADGE_COLORS = {outdated_major: 'red', outdated_minor: 'orange', outdated_patch: 'yellow', ok: '#4c1', unknown: '#555'} unless defined?(BADGE_COLORS)
+  BADGE_LAYOUT = {
+    ok: '#4c1',
+    outdated_patch: 'yellow',
+    outdated_minor: 'orange',
+    outdated_major: 'red',
+    unknown: '#555'
+  } unless defined?(BADGE_LAYOUT) # Order is significant
 end
 
 class BadgeWorker < GenericWorker
-  STATUS_TYPES = [:ok, :outdated_patch, :outdated_minor, :outdated_major, :unknown]  # Order is significant
 
   def perform(identifier)
     log_info 'Running Worker!'
     dependencies = get_from_store(identifier) # What if this fails?
-    dependencies = STATUS_TYPES.map { |k| [k, dependencies[k]] }.to_h.
+    dependencies = BADGE_LAYOUT.keys.map { |k| [k, dependencies[k]] }.to_h.
       delete_if {|type, number| number == 0}
 
     add_to_store("svg-#{identifier}", ERB.new(CuttingEdge::BADGE_TEMPLATE).result_with_hash(

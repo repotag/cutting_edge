fc957b42eef58b2da18f7630e8cdd34700395650
Dawa Ometto <d.ometto@gmail.com>
2020-09-12 15:45:21 +0200

    Wrap locations in dependency hash (#36)

--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -2,52 +2,55 @@ def mock_dependencies(name)
   {
     'gollum' =>
     {
-      'gollum.gemspec' =>
-        {:outdated_major=>
-          [{:name=>'mustache',
-            :required=>'>= 0.99.5, < 1.0.0',
-            :latest=>'1.1.1',
-            :type=>:runtime},
-           {:name=>'octicons',
-            :required=>'~> 8.5',
-            :latest=>'9.6.0',
-            :type=>:runtime}],
-         :outdated_minor=>
-          [{:name=>'kramdown-parser-gfm',
-            :required=>'~> 1.0.0',
-            :latest=>'1.1.0',
-            :type=>:runtime}],
-         :outdated_patch=>[],
-         :ok=>
-          [
-            {:name=>'gollum-lib',
-             :required=>'~> 5.0',
-             :latest=>'5.0.3',
-             :type=>:runtime},
-            {:name => 'foobar',
-             :required => '= 1.0',
-             :latest => '1.0',
-             :type => :runtime
-           }
-         ],
-          :no_requirement=>[],
-          :unknown=>[]},
-       'Gemfile' =>
+      :locations =>
+      {
+        'gollum.gemspec' =>
+          {:outdated_major=>
+            [{:name=>'mustache',
+              :required=>'>= 0.99.5, < 1.0.0',
+              :latest=>'1.1.1',
+              :type=>:runtime},
+             {:name=>'octicons',
+              :required=>'~> 8.5',
+              :latest=>'9.6.0',
+              :type=>:runtime}],
+           :outdated_minor=>
+            [{:name=>'kramdown-parser-gfm',
+              :required=>'~> 1.0.0',
+              :latest=>'1.1.0',
+              :type=>:runtime}],
+           :outdated_patch=>[],
+           :ok=>
+            [
+              {:name=>'gollum-lib',
+               :required=>'~> 5.0',
+               :latest=>'5.0.3',
+               :type=>:runtime},
+              {:name => 'foobar',
+               :required => '= 1.0',
+               :latest => '1.0',
+               :type => :runtime
+             }
+           ],
+            :no_requirement=>[],
+            :unknown=>[]},
+        'Gemfile' =>
           {
-           :outdated_major=>[
+            :outdated_major=>[
               {
                 :name=>'rake',
                 :required=>'~> 12.3, >= 12.3.3',
                 :latest=>'13.0.1',
                 :type=>:runtime
               }],
-           :outdated_minor=>[],
-           :outdated_patch=>[],
-           :ok=>
-            [{:name=>'warbler', :required=>'>= 0', :latest=>'2.0.5', :type=>:runtime}],
-           :no_requirement=>[],
-           :unknown=>[]
-         },
+            :outdated_minor=>[],
+            :outdated_patch=>[],
+            :ok=>
+             [{:name=>'warbler', :required=>'>= 0', :latest=>'2.0.5', :type=>:runtime}],
+            :no_requirement=>[],
+            :unknown=>[]
+          },
+      },
       :ok=>3,
       :outdated=>:outdated_major,
       :outdated_major=>3,
@@ -59,45 +62,48 @@ def mock_dependencies(name)
     },
     'gollum-updated' =>
       {
-        'Gemfile'=>
-          {:no_requirement=>[],
-           :ok=>
-            [{:latest=>'3.0.5',
-              :name=>'warbler',
-              :required=>'>= 0',
-              :type=>:runtime}],
-           :outdated_major=>
-            [{:latest=>'14.0.1',
-              :name=>'rake',
-              :required=>'~> 12.3, >= 12.3.3',
-              :type=>:runtime}],
-           :outdated_minor=>[],
-           :outdated_patch=>[],
-           :unknown=>[]},
-       'gollum.gemspec'=>
-          {:no_requirement=>[],
-           :ok=>[],
-           :outdated_major=>
-            [{:latest=>'2.1.1',
-              :name=>'mustache',
-              :required=>'>= 0.99.5, < 1.0.0',
-              :type=>:runtime},
-             {:latest=>'10.6.0',
-              :name=>'octicons',
-              :required=>'~> 8.5',
-              :type=>:runtime},
-             {:latest=>'2.1.0',
-              :name=>'kramdown-parser-gfm',
-              :required=>'~> 1.0.0',
-              :type=>:runtime},
-             {:latest=>'6.0.3',
-              :name=>'gollum-lib',
-              :required=>'~> 5.0',
-              :type=>:runtime},
-             {:latest=>'2.0', :name=>'foobar', :required=>'= 1.0', :type=>:runtime}],
-           :outdated_minor=>[],
-           :outdated_patch=>[],
-           :unknown=>[]},
+        :locations =>
+        {
+          'Gemfile'=>
+            {:no_requirement=>[],
+             :ok=>
+              [{:latest=>'3.0.5',
+                :name=>'warbler',
+                :required=>'>= 0',
+                :type=>:runtime}],
+             :outdated_major=>
+              [{:latest=>'14.0.1',
+                :name=>'rake',
+                :required=>'~> 12.3, >= 12.3.3',
+                :type=>:runtime}],
+             :outdated_minor=>[],
+             :outdated_patch=>[],
+             :unknown=>[]},
+         'gollum.gemspec'=>
+            {:no_requirement=>[],
+             :ok=>[],
+             :outdated_major=>
+              [{:latest=>'2.1.1',
+                :name=>'mustache',
+                :required=>'>= 0.99.5, < 1.0.0',
+                :type=>:runtime},
+               {:latest=>'10.6.0',
+                :name=>'octicons',
+                :required=>'~> 8.5',
+                :type=>:runtime},
+               {:latest=>'2.1.0',
+                :name=>'kramdown-parser-gfm',
+                :required=>'~> 1.0.0',
+                :type=>:runtime},
+               {:latest=>'6.0.3',
+                :name=>'gollum-lib',
+                :required=>'~> 5.0',
+                :type=>:runtime},
+               {:latest=>'2.0', :name=>'foobar', :required=>'= 1.0', :type=>:runtime}],
+             :outdated_minor=>[],
+             :outdated_patch=>[],
+             :unknown=>[]},
+         },
          :no_requirement=>0,
          :ok=>1,
          :outdated=>:outdated_major,
@@ -117,7 +123,7 @@ def mock_dependencies_no_stats(name)
 end
 
 def mock_fetched_requirements(name, location, update_latest = false)
-  dependencies = mock_dependencies(name)[location]
+  dependencies = mock_dependencies(name)[:locations][location]
   results = []
   dependencies.each_value do |dependency_type|
     dependency_type.each do |dependency|

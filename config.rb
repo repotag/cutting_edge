1df475a13edc5b0d680f891f6cf32d288c7e57da
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2020-09-13 19:19:14 +0200

    More examples in config.rb

--- config.rb
+++ config.rb
@@ -1,12 +1,17 @@
 # # Example config.rb for CuttingEdge
+# # All the settings below are purely optional: they all have sane defaults.  
 # 
 # module CuttingEdge
 #   REFRESH_SCHEDULE = '1h'  # How often to run workers to check for changes to dependency status. Examples of valid values: '10d', '10m', etc.
 #   SERVER_HOST = 'mydependencymonitoring.com' # At what domain is this CuttingEdge instance running? Defaults to 'localhost'
 #   SERVER_URL = "https://#{SERVER_HOST}" # The URL used to refer to this CuttingEdge instance, for instance in e-mails. Defaults to 'http://#{SERVER_HOST}' 
 # 
-#   MAIL_TO = false # Default address to send email to. If set to false, don't send any e-mails except for repositories that have their 'email:' attribute set in projects.yml
+#   MAIL_TO = 'mydeps@mymail.com'  # Default address to send email to. If set to false (=default!), don't send any e-mails except for repositories that have their 'email:' attribute set in projects.yml
 #   MAIL_FROM = "cutting_edge@#{SERVER_HOST}" # From Address used for sending e-mails. Default: "cutting_edge@#{SERVER_HOST}" 
+#   MAIL_TEMPLATE = <<EOF
+#     Define your own ERB template for e-mail updates from CuttingEdge here!
+#     See lib/cutting_edge/templates/mail.html.erb for the default template, and the available variables.
+# EOF
 # 
 #   LAST_VERSION_TIMEOUT = 5 # Number of seconds after which to fail when trying to determine the latest version for a dependency.
 # 
@@ -18,7 +23,7 @@
 #    unknown: '#9f9f9f'
 #   } # Redefine the colors used in SVG badges
 # 
-#   # The following will allow you to define projects on your own GitLab instance in projects.yml
+#   # The following will allow you to define projects on your own GitLab server in projects.yml
 #   # If you want to use the key 'myserver' in projects.yml, your repository class must be named MyserverRepository
 #   require './lib/cutting_edge/repo.rb'
 #   class MyserverRepository < GitlabRepository # The naming here is .

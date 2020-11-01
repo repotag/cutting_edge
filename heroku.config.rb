8f4dced458e8fb40e05b2e2f5b3099fd9538be29
Dawa Ometto <d.ometto@gmail.com>
2020-11-01 20:21:23 +0100

    Update heroku.config.rb
    Fix heroku config

--- heroku.config.rb
+++ heroku.config.rb
@@ -1,6 +1,7 @@
 # Default settings required for Heroku deployment
 
 # Configure mail server settings
+require 'mail'
 Mail.defaults do
   delivery_method :smtp, address: ENV['MAILGUN_SMTP_SERVER'], port: ENV['MAILGUN_SMTP_PORT'], user_name: ENV['MAILGUN_SMTP_LOGIN'], password: ENV['MAILGUN_SMTP_PASSWORD']
 end
@@ -8,6 +9,7 @@ end
 module CuttingEdge
   SERVER_HOST = ENV['HEROKU_APP_NAME'] # At what domain is this CuttingEdge instance running?
   SERVER_URL = "https://#{SERVER_HOST}"
+  MAIL_TO = ENV['CUTTING_EDGE_MAIL_TO'] if ENV['CUTTING_EDGE_MAIL_TO']
   
   # Your additional configuration goes here.
   # If you are going to host the repository containing this file publically (e.g. on GitHub), please read:

e538ba7b338672fc6868adaa8f2345dd69807bae
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-10-15 12:59:24 +0200

    Update heroku.config.rb

--- heroku.config.rb
+++ heroku.config.rb
@@ -4,7 +4,16 @@ module CuttingEdge
   SERVER_HOST = "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" # At what domain is this CuttingEdge instance running?
   SERVER_URL = "https://#{SERVER_HOST}"
   MAIL_TO = ENV['CUTTING_EDGE_MAIL_TO'] if ENV['CUTTING_EDGE_MAIL_TO']
-  
+  if ENV['REDIS_TLS_URL']
+    require 'moneta'
+    require 'openssl'
+    STORE = ::Moneta.new(:Redis,
+      url: ENV['REDIS_TLS_URL'],
+      ssl_params: {
+        verify_mode: OpenSSL::SSL::VERIFY_NONE
+      }
+    )
+  end
   # Your additional configuration goes here.
   # If you are going to host the repository containing this file publically (e.g. on GitHub), please read:
   # https://github.com/repotag/cutting_edge/blob/master/README.md#Defining-repositories-in-configrb
